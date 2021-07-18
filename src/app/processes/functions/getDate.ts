39e09fd12497629510d15ab1f27927e2ddff7c08
Aaron Fletcher <afletcher53@gmail.com>
2021-07-18 20:49:56 +0100

    reducing complexity

--- src/app/processes/functions/getDate.ts
+++ src/app/processes/functions/getDate.ts
@@ -5,28 +5,7 @@
  */
 
 const getDate = (articleData) => {
-  if (typeof articleData.message['published-online'] !== 'undefined') {
-    if (!articleData.message['published-online']['date-parts'][0][2]) {
-      var day = 1;
-    } else {
-      day = articleData.message['published-online']['date-parts'][0][2];
-    }
-
-    if (articleData.message['published-online']['date-parts'][0][1]) {
-      var month = articleData.message['published-online']['date-parts'][0][1];
-    } else {
-      month = 1;
-    }
-
-    if (articleData.message['published-online']['date-parts'][0][0]) {
-      var year = Number(
-        articleData.message['published-online']['date-parts'][0][0] - 1
-      );
-    } else {
-      year = 1;
-    }
-    var publishedOnlineDate = new Date(Date.UTC(year, month, day));
-  }
+  var { day, month, year, publishedOnlineDate } = publishedOnline(articleData);
 
   if (typeof articleData.message['published-print'] !== 'undefined') {
     if (!articleData.message['published-print']['date-parts'][0][2]) {
@@ -54,3 +33,29 @@ const getDate = (articleData) => {
 };
 
 export default getDate;
+
+function publishedOnline(articleData: any) {
+  if (typeof articleData.message['published-online'] !== 'undefined') {
+    if (!articleData.message['published-online']['date-parts'][0][2]) {
+      var day = 1;
+    } else {
+      day = articleData.message['published-online']['date-parts'][0][2];
+    }
+
+    if (articleData.message['published-online']['date-parts'][0][1]) {
+      var month = articleData.message['published-online']['date-parts'][0][1];
+    } else {
+      month = 1;
+    }
+
+    if (articleData.message['published-online']['date-parts'][0][0]) {
+      var year = Number(
+        articleData.message['published-online']['date-parts'][0][0] - 1
+      );
+    } else {
+      year = 1;
+    }
+    var publishedOnlineDate = new Date(Date.UTC(year, month, day));
+  }
+  return { day, month, year, publishedOnlineDate };
+}
